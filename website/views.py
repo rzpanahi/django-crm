7d4c78b56624a00ae3b855ec7e696861e7d86d8c
Reza Panahi <rezapanahi1984@proton.me>
2023-11-17 19:42:19 +0330

    add login for users

--- website/views.py
+++ website/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == "POST":
+        # user is not logged in
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # authenticatet
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in successfully!")
+        else:
+            messages.success(request, "username or password incorrect")
+
+        return redirect("home")
+
+    elif request.method == "GET":
+        # user is logged in
+        return render(request, "home.html", {})
+
+
+def logout_user(request):
+    pass
